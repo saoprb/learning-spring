8cdb10287da98463d57c19862d9a367cff390fd9
Sao Paulo Boco <saoprb@gmail.com>
2017-03-15 23:24:22 +0800

    Add initial commit for hibernate jpa example.

---
+++ hibernate-jpa-example/src/main/java/com/sao/spring/learning/Application.java
@@ -0,0 +1,29 @@
+package com.sao.spring.learning;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Sao Paulo Boco on 3/15/2017.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return x -> {
+          System.out.println("Hello world");
+        };
+    }
+}
