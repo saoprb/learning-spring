5266c7d4de06607c6a7ba4f0b79f7991947a5a7e
Sao Paulo Boco <saoprb@gmail.com>
2017-04-02 15:24:45 +0800

    Initial commit for twitter stream example.

---
+++ twitter-stream/src/main/java/com/sao/spring/learning/Application.java
@@ -0,0 +1,31 @@
+package com.sao.spring.learning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by saopr on 4/2/2017.
+ */
+@SpringBootApplication
+@ComponentScan
+    public class Application {
+
+    Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+          logger.info("Hello world");
+        };
+    }
+}
